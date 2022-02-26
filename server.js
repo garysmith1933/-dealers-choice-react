f3cdb692e7544d46a61afbdea750087b4d667539
Gary <garysmith193358@gmail.com>
2022-02-26 23:08:55 +0000

    all done but extra credit

--- server.js
+++ server.js
@@ -13,15 +13,16 @@ const Team = sequelize.define('team', {
     }
 })
 
-
-
 const express = require("express")
 const app = express()
+const path = require('path')
+
+app.get('/', async(req,res) => res.sendFile(path.join(__dirname, 'index.html')))
 
 
 app.get('/api/teams', async(req,res,next) => {
     try {
-        
+        res.send(await Team.findAll())
     } catch(err) {
         
     }
